932ab1ba121acc529f6bb6c92e980dda3c77f3a4
VishalTyagi8974 <115866008+VishalTyagi8974@users.noreply.github.com>
2025-05-14 17:43:08 +0530

    v2

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import Home from "./home/Home";
+import ITRFiling from "./taxAndCompilance/itrFiling/ITRFiling"
+import ITRNoticeAppeal from "./taxAndCompilance/itrNoticeAppeal/ITRNoticeAppeal"
+import TaxPlanning from "./taxAndCompilance/taxPlanning/TaxPlanning"
+import TrademarkRegistration from "./trademark/trademarkRegistration/TrademarkRegistration";
+import TrademarkRenewal from "./trademark/trademarkRenewal/TrademarkRenewal";
+import CopyrightRegistration from "./trademark/copyrightRegistration/CopyrightRegistration";
+import FSSAIRegistration from "./licenses/fssaiRegistration/FSSAIRegistration";
+import FSSAIRenewal from "./licenses/fssaiRenewal/FSSAIRenewal"
+import ISOCertification from "./others/isoCertification/ISOCertification";
+import GSTRegistration from "./taxAndCompilance/gstRegistration/GSTRegistration";
+import GSTReturnFiling from "./taxAndCompilance/gstReturnFiling/GSTReturnFiling";
+
+
+export { Home, ITRFiling, ITRNoticeAppeal, TaxPlanning, TrademarkRegistration, TrademarkRenewal, CopyrightRegistration, FSSAIRegistration, FSSAIRenewal, ISOCertification, GSTRegistration, GSTReturnFiling }
